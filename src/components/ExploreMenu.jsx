f7a93f8269bed642d3dde715deb879ff769c102d
Nation47 <hananiyusuph98@gmail.com>
2024-09-19 23:00:47 +0300

    update on scrollbar

--- src/components/ExploreMenu.jsx
+++ src/components/ExploreMenu.jsx
@@ -6,10 +6,10 @@ const ExploreMenu = ({category, setCategory}) => {
             <div className="flex flex-col gap-5 m-auto max-w-6xl my-6 ">
                 <h2 className="font-bold text-pink-900 text-2xl">Tembelea Menu</h2>
                 <p className="max-w-xl text-gray-800">Tembelea Menu Tofauti za Chakula Kila Siku, Ukitumia Mfumo Wetu wa Kuagiza Haraka na Kwa Uhakika</p>
-                <div className="flex justify-between items-center my-5 mx-0 snap-x overflow-x-scroll gap-6 text-center">
+                <div className="flex justify-between items-center my-5 mx-0  overflow-x-scroll  gap-6 text-center [&::-webkit-scrollbar]:hidden [-ms-overflow-style:none] [scrollbar-width:none]">
                     {menuList.map((item,index) => {
                         return(
-                            <div className="snap-center" onClick={() => setCategory(prev => prev === item.name? "All": item.name)} key={index}>
+                            <div className="" onClick={() => setCategory(prev => prev === item.name? "All": item.name)} key={index}>
                                 <img src={item.img} alt="" className={category === item.name ?"cursor-pointer w-36 min-w-20 rounded-full border-4 border-pink-900":"cursor-pointer w-36 min-w-20 rounded-full"} />
                                 <p className="mt-2.5 text-pink-950 cursor-pointer text-base">{item.name}</p>
                             </div>
